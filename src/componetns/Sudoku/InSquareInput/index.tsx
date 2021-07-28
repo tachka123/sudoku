6f3b35e50f9413ca30f0240d4f04b341a7a4c434
Alexander <123tachka123@gmail.com>
2021-07-28 16:29:55 +0300

    Fixes

--- src/componetns/Sudoku/InSquareInput/index.tsx
+++ src/componetns/Sudoku/InSquareInput/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import s from "./s.module.css";
 
 const InSquareInput = () => {
-  return <input className={s.inputSquare}></input>;
+  return <input maxLength={1} type="number" className={s.inputSquare}></input>;
 };
 
 export default InSquareInput;
