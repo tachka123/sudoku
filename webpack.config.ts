c4b291c351697f1691323bfaeb9f3a5703b153b1
Alexander <123tachka123@gmail.com>
2021-07-28 14:21:32 +0300

    Created Webpack and configurated

---
+++ webpack.config.ts
@@ -0,0 +1,40 @@
+import path from "path";
+import webpack from "webpack";
+
+const config = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 4000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+};
+
+export default config;
