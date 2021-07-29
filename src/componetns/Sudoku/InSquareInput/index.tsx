864ad3593fb316830d5c436de9a1052dcef16379
Alexander <123tachka123@gmail.com>
2021-07-30 02:15:14 +0300

    Deleted unnecessary

--- src/componetns/Sudoku/InSquareInput/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import s from "./s.module.css";
-
-const InSquareInput = () => {
-  return <input type="number" className={s.inputSquare}></input>;
-};
-
-export default InSquareInput;
