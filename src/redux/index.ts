9cd3c2088ab8dedefee51c8a6d91ef53971a6120
Alexander <123tachka123@gmail.com>
2021-07-30 02:16:45 +0300

    Sudoku Types, Generating Default Sudoku,Sudoku Reducer

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,12 +1,20 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 import ComplexityReducer from "./complexity";
+import SudokyReducer from "./matrix";
+
+const middlwares = [thunk];
 
 const appReducers = combineReducers({
   complexity: ComplexityReducer,
+  sudoky: SudokyReducer,
 });
 
-const store = createStore(appReducers, composeWithDevTools());
+const store = createStore(
+  appReducers,
+  composeWithDevTools(applyMiddleware(...middlwares))
+);
 
 export type IStateRedux = ReturnType<typeof store.getState>;
 
