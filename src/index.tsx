95e6b717394891637078322d5d674e19e145e26b
Alexander <123tachka123@gmail.com>
2021-07-30 12:00:43 +0300

    Changed to HashRouter for convenience

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./componetns/App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById("root")
 );
