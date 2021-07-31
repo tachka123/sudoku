b83715cb934947ee99dd76e406e755bb03641eee
Alexander <123tachka123@gmail.com>
2021-07-31 15:54:19 +0300

    Utils moved to utils folder

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,29 +1,97 @@
-function shuffle(arr: number[]) {
-  const ret = [];
-  while (ret.length < arr.length) {
-    const x = arr[Math.floor(Number(Math.random() * arr.length))];
-    if (!(ret.indexOf(x) >= 0)) ret.push(x);
-  }
-  return ret;
+import sudoku from "sudoku";
+import { createDefault } from "../redux/matrix/state";
+import { ISquarePoint, SquareSudoky } from "../types/helpers";
+
+export function generateSudoky() {
+  const result = sudoku.solvepuzzle(sudoku.makepuzzle());
+  const sud = createDefault();
+  let index = 0;
+  sud.forEach((i, inde) =>
+    i.forEach((j, jd) => {
+      sud[inde][jd] = {
+        ...sud[inde][jd],
+        value: result[index] === null ? "" : String(result[index] + 1),
+        disabled: result[index] !== null,
+      };
+      index++;
+    })
+  );
+  return sud;
 }
 
-export default function getSudoku() {
-  const sudoku: number[][] = [];
-  const arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  sudoku.push(arr);
-  for (let i = 1; i < 9; i++) {
-    while (sudoku.length <= i) {
-      const newarr = shuffle(arr);
-      let b = false;
-      for (let j = 0; j < arr.length; j++) {
-        for (let k = 0; k < i; k++) {
-          if (sudoku[k].indexOf(newarr[j]) == j) b = true;
+const checkSquareForReapiating = (
+  val: ISquarePoint,
+  arr: SquareSudoky
+): number => {
+  const Xmin = [6, 3, 0];
+  const Xmax = [2, 5, 8];
+  const Ymin = [3, 0];
+  const Ymax = [2, 5];
+
+  const x = [
+    Xmin.find((item) => item <= val.x),
+    Xmax.find((item) => item >= val.x),
+  ];
+  const y = [
+    Ymin.find((item) => item <= val.y),
+    Ymax.find((item) => item >= val.y),
+  ];
+  if (
+    typeof x[0] !== "number" ||
+    typeof x[1] !== "number" ||
+    typeof y[0] !== "number" ||
+    typeof y[1] !== "number"
+  ) {
+    return 0;
+  }
+  let counter = 0;
+
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 9; j++) {
+      const current = arr[i][j];
+      if (
+        x[0] <= current.x &&
+        current.x <= x[1] &&
+        current.y <= y[1] &&
+        y[0] <= current.y &&
+        current.value !== ""
+      ) {
+        if (
+          current.x !== val.x &&
+          current.y !== val.y &&
+          current.value === val.value
+        ) {
+          return 2;
         }
       }
-      if (!b) {
-        sudoku.push(newarr);
-      }
+      if (counter > 1) return counter;
     }
   }
-  return sudoku;
-}
+  return counter;
+};
+
+export const checkPointForCorrection = (
+  mapped: SquareSudoky,
+  currentPoint: ISquarePoint
+): boolean => {
+  const xVals = mapped.map((item) =>
+    item.filter((it) => it.x === currentPoint.x && it.value !== "")
+  );
+  const yVals = mapped.map((item) =>
+    item.filter((it) => it.y === currentPoint.y && it.value !== "")
+  );
+  const checkedX = checkForReapiatingByXY(currentPoint.value, xVals);
+  const checkedY = checkForReapiatingByXY(currentPoint.value, yVals);
+  const checkSquare = checkSquareForReapiating(currentPoint, mapped);
+  return checkedX > 1 || checkedY > 1 || checkSquare > 1;
+};
+
+export const checkForReapiatingByXY = (val: string, arr: ISquarePoint[][]) => {
+  let counter = 0;
+  arr.forEach((el) => {
+    el.forEach((elem) => {
+      elem.value === val && counter++;
+    });
+  });
+  return counter;
+};
